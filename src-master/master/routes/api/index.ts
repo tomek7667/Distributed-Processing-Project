77b8f76776a977d64dfefbf470ee59774710943f
tomek7667 <tomek.aabiot@gmail.com>
2023-05-19 16:28:22 +0200

    Master: remove comments, remove } from ping

--- src-master/master/routes/api/index.ts
+++ src-master/master/routes/api/index.ts
@@ -5,13 +5,8 @@ const router = express.Router();
 router.get("/ping", (_req, res) => {
 	res.json({
 		success: true,
-		data: `pong! at ${new Date().toLocaleString()}}`,
+		data: `pong! at ${new Date().toLocaleString()}`,
 	});
 });
 
 export default router;
-// 
-// b 
-// c
-// ...
-// z
